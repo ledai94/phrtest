40891bc9b78f51e5615732fb1e233e1527f41a25
ledai94 <brian3dcnbo@gmail.com>
2020-11-30 22:33:22 +0700

    28 nuxtserveinit cach 1

--- store/index.js
+++ store/index.js
@@ -14,45 +14,44 @@ const createStore = () => {
         return new Promise((resolve, reject) => {
           // eslint-disable-next-line nuxt/no-timing-in-fetch-data
           setTimeout(() => {
-            resolve({
-              decks: [
-                {
-                  _id: 1,
-                  name: `learn english  `,
-                  description:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-                  thumbnail:
-                    'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
-                },
-                {
-                  _id: 2,
-                  name: `learn english `,
-                  description:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-                  thumbnail:
-                    'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
-                },
-                {
-                  _id: 3,
-                  name: `learn english `,
-                  description:
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-                  thumbnail:
-                    'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
-                },
-              ],
-            })
+            vuexContext.commit('setDecks', [
+              {
+                _id: 1,
+                name: `learn english  `,
+                description:
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+                thumbnail:
+                  'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
+              },
+              {
+                _id: 2,
+                name: `learn english `,
+                description:
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+                thumbnail:
+                  'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
+              },
+              {
+                _id: 3,
+                name: `learn english `,
+                description:
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+                thumbnail:
+                  'https://idc.edu/wp-content/uploads/2018/12/15-Techniques-for-Learning-English-Vocabulary-850x390.jpg',
+              },
+            ])
+            resolve()
           }, 1500)
           // reject(new Error())
         })
-          .then((data) => {
-            vuexContext.commit('setDecks', data.decks)
-          })
-          .catch((e) => {
-            // eslint-disable-next-line no-console
-            console.log(e)
-            // context.error(e)
-          })
+        // .then((data) => {
+        //   vuexContext.commit('setDecks', data.decks)
+        // })
+        // .catch((e) => {
+        //   // eslint-disable-next-line no-console
+        //   console.log(e)
+        //   // context.error(e)
+        // })
       },
       setDecks(vuexContext, decks) {
         vuexContext.commit('setDecks', decks)
